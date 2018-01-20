99bc4c1dde79d44050055ba6171ae748edbcc59a
Samuel Fabrizio <samuelfabrizio@gmail.com>
2018-01-20 16:39:08 +0100

    [ADD] basic logic to render backgrounds

---
+++ public/js/loader.js
@@ -0,0 +1,17 @@
+
+export function loadImage (url) {
+    return new Promise( resolve => {
+        const image = new Image();
+        image.addEventListener("load", () => {
+            resolve(image);
+        })
+        image.src = url;
+    })
+}
+
+export function loadLevel(name) {
+    return fetch(`/levels/${name}.json`)
+        .then( r => {
+            return r.json();
+        })
+}
