991e8c814f5de50e3bec911c6d3cc19e71b0af07
Samuel Fabrizio <samuelfabrizio@gmail.com>
2018-01-27 18:09:43 +0100

    [ADD] Keyboard Handler

---
+++ public/js/KeyboardHandler.js
@@ -0,0 +1,35 @@
+const PRESSED = 1;
+const RELEASED = 0;
+
+export default class KeyboardHandler {
+    constructor(window) {
+        this.keyMapping = new Map();
+        this.keyState = new Map();
+        this.listenTo(window);
+    }
+
+    addKeyBinding(keyName, callback) {
+        this.keyMapping.set(keyName, callback);
+    }
+
+    handleEvent(event) {
+        const {code, type} = event
+        
+        if ( !this.keyMapping.has(code) ) { return; }
+        event.preventDefault();
+        
+        const keyState = type === "keydown" ? PRESSED : RELEASED;
+        if (keyState === this.keyState.get(code) ) { return; }
+        this.keyState.set(code, keyState)
+
+        const callback = this.keyMapping.get(code);
+        callback(keyState);
+    }
+
+    listenTo(window) {
+        ['keydown', 'keyup'].forEach( eventName => {
+            window.addEventListener(eventName, this.handleEvent.bind(this) );
+        })
+    }
+
+}
