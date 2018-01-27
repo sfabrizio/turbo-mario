5c451bcc9aa0e41de86bdb6891eb6d90f70b9c9b
Samuel Fabrizio <samuelfabrizio@gmail.com>
2018-01-27 18:11:38 +0100

    [ADD] Implement entities Traits

--- public/js/Entity.js
+++ public/js/Entity.js
@@ -1,8 +1,31 @@
 import {Vec2} from './math.js'
 
+export class Trait {
+    constructor(name) {
+        this.NAME = name;
+    }
+
+    update () {
+        console.warn('Update method not implement on this Trait', this);
+    }
+
+}
+
 export default class Entity {
     constructor(){
         this.vel = new Vec2(0, 0);
         this.pos = new Vec2(0, 0);
+        this.traits = [];
+    }
+
+    addTrait (trait) {
+        this.traits.push(trait);
+        this[trait.NAME] = trait;
+    }
+
+    update (deltaTime) {
+        this.traits.forEach( trait => {
+            trait.update(this, deltaTime);
+        })
     }
 }
---
+++ public/js/traits/Jump.js
@@ -0,0 +1,26 @@
+import {Trait} from '../Entity.js';
+
+export default class Jump extends Trait {
+    constructor() {
+        super('jump');
+
+        this.duration = 0.5;
+        this.velocity = 200;
+        this.engageTime = 0;
+    }
+
+    update(entity, deltaTime) {
+        if (this.engageTime > 0 ) {
+            entity.vel.y = -this.velocity
+            this.engageTime -= deltaTime;
+        }
+    }
+
+    start(entity) {
+      this.engageTime = this.duration;
+    }
+    
+    cancel(entity) {
+      this.engageTime = this.duration;
+    }
+} 
