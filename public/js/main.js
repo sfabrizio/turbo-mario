014faaddeccea5f3b79ec9d840757fc399472cf2
Samuel Fabrizio <samuelfabrizio@gmail.com>
2018-01-28 19:05:48 +0100

    [ADD] mouse debug for mario position

--- public/js/main.js
+++ public/js/main.js
@@ -6,22 +6,22 @@ import {createMario} from './entities.js'
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
-context.scale(2,2)
+// context.scale(2,2)
 
-
+const mouseDebug = true;
 
 Promise.all([
     createMario(),
     loadLevel('1-1')
 ])
 .then( ([mario, level]) => {
-
     const gravity = 800;
     mario.pos.set(64, 5);
 
     level.entities.add(mario);
 
     const input = new KeyboardHandler(window);
+    
     input.addKeyBinding('Space', keyState => {
         if (keyState) {
             mario.jump.start();
@@ -29,6 +29,17 @@ Promise.all([
             mario.jump.cancel();
         }
     });
+
+    if (mouseDebug) {
+        ['mousedown','mousemove'].forEach( eventName => {
+            canvas.addEventListener(eventName, event => {
+                if (event.buttons === 1) {
+                    mario.vel.set(0, 0);
+                    mario.pos.set(event.offsetX, event.offsetY);
+                }
+            })
+        })
+    }
     
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime) {
